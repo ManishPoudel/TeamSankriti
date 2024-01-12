8d9e9343075080ff2fb4d3bc833dbb7dfbcfd630
Jyoti <chatautjyoti7@gmail.com>
2024-01-12 13:00:52 +0545

    new commit from joyti

---
+++ users/jyotiChataut/parking-spa/src/views/auth/changePassword.vue
@@ -0,0 +1,118 @@
+<template>
+  <!-- <ve-notifications /> -->
+  <div class="container mx-auto px-4 h-full">
+    <div class="flex content-center items-center justify-center h-full">
+      <div class="w-full lg:w-4/12 px-4">
+        <div
+          class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+        >
+          <div class="rounded-t mb-0 px-6 py-6">
+            <div class="text-center mb-3">
+              <h6 class="text-blueGray-500 text-sm font-bold">
+                Change Password
+              </h6>
+            </div>
+
+            <hr class="mt-6 border-b-1 border-blueGray-300" />
+          </div>
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <form @submit.prevent="submit">
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                 Old Password
+                </label>
+                <input
+                  type="password"
+                  v-model="oldPassword"
+                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  v-model="newPassword"
+                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  v-model="confirmPassword"
+                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div class="text-center mt-6">
+                <button
+                  class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                  type="submit"
+                  @click="changePassword"
+                >
+                  CHange Password
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="flex flex-wrap mt-6 relative">
+          <div class="w-1/2">
+            <a href="javascript:void(0)" class="text-blueGray-200">
+              <small>Forgot password?</small>
+            </a>
+          </div>
+          <div class="w-1/2 text-right">
+            <router-link to="/auth/register" class="text-blueGray-200">
+              <small>Create new account</small>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import github from "../../assets/img/github.svg";
+import google from "../../assets/img/google.svg";
+// import Notifications from "../../components/BaseToasts.vue";
+
+export default {
+  components: {
+    // "ve-notifications": Notifications,
+  },
+  data() {
+    return {
+      github,
+      google,
+      oldPassword: null,
+      newPassword: null,
+      confirmPassword: null,
+    };
+  },
+  created() {
+    this.csrf_cookie();
+  },
+  methods: {
+     changePassword() {
+      
+    },
+  },
+};
+</script>
---
+++ users/jyotiChataut/parking-spa/src/views/auth/forgotPassword.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="w-1/2">
+      <div
+        class="relative flex-col item-end min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+      >
+        <form @submit.prevent="submit">
+          <label for="email">email</label>
+          <input v-model="email" type="text" name="email" placeholder="Enter your email here..."/>
+          <div>
+            <button type="submit" @click="forgotPassword()">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            email:null
+        }
+    },
+    methods:{
+        forgotPassword(){
+            console.log('Jyoti')
+        }
+    }
+}
+</script>
